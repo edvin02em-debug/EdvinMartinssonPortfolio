191473b603bd4a27abfaeccdfb823bd6e6868fee
edvin02em-debug <edvin02.em@gmail.com>
2026-03-09 11:42:05 +0100

    Adjust threshold for last section activation in TOC
    Increase scroll threshold for activating last section in TOC.

--- assets/js/toc.js
+++ assets/js/toc.js
@@ -24,7 +24,7 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     /* activate last section near page bottom */
-    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight - 100) {
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight - 300) {
       currentSection = sections[sections.length - 1];
     }
 
