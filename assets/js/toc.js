542a16bb26f74ee12f7201f6071551faef0f5cc4
edvin02em-debug <edvin02.em@gmail.com>
2026-03-09 11:51:38 +0100

    Update toc.js

--- assets/js/toc.js
+++ assets/js/toc.js
@@ -24,7 +24,7 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     /* activate last section near page bottom */
-    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight - 600) {
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight - 400) {
       currentSection = sections[sections.length - 1];
     }
 
